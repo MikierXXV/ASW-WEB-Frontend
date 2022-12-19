40a2cb8923bedab11ed4d3e86455572470dd1750
marc.rodriguez.martin <marc.rodriguez.martin@estudiantat.upc.edu>
2022-12-19 17:14:50 +0100

    feature #77 - Added code for comment list and submission list

---
+++ src/components/submissions/ListSubmissions.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import Submission from "./Submission";
+
+export default class ListSubmissions extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            submissions: props.submissions
+        };
+    }
+
+    render() {
+        let index = 0;
+        const { submissions } = this.state;
+
+        return (
+            <div className="row ml-1">
+                <div className="col">
+                    { submissions.map((submission) => {
+                        index++;
+                        return <Submission key={submission.id} index={index} submission={submission} />
+                    })
+                    }
+                </div>
+            </div>
+        );
+    }
+}
